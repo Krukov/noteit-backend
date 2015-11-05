57cfaab754998c93c053512ae7ea7f5652251d72
Krukov <glebov.ru@gmail.com>
2015-11-05 20:11:21 +0300

    Fix local settings

--- send/urls_local.py
+++ send/urls_local.py
@@ -6,7 +6,8 @@ from django.conf.urls import include, url, static
 from django.contrib import admin
 
 from .urls import urlpatterns
-urlpatterns = urlpatterns + [
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-] + static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
+] + urlpatterns \
+  + static.static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- send/users/urls.py
+++ send/users/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 
 from .views import QuestionView, drop_token, get_token
 
+
 urlpatterns = [
     url(r'^(?P<uuid>[0-9a-zA-Z_-]+)/', QuestionView.as_view(), name='question'),
     url(r'^get_token', get_token, name='get_token'),
